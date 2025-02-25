e4e07538789e46d9f17a91f1aca5df7cd45b73f7
Md Adnan qaisar <staryounger012@gmail.com>
2025-02-25 16:58:17 +0530

    Add files via upload

---
+++ hellowrectangle.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+public class hellowRectangle {
+    public static void hellow_rectangle(int totRows, int totCols) {
+        //outer loop
+        for(int i=1; i<=totRows; i++) {
+            // inner - colums
+            for(int j=1; j<=totCols; j++) {
+                if(i == 1 || i == totRows || j == 1 || j == totCols) {
+                    // bounderies cell
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+
+        }
+
+    }
+    public static void main(String args[]) {
+        hellow_rectangle(10, 7);
+
+    }
+}
+
+
+
